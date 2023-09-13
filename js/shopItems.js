3ffd7c29ea5e5b55d94e70f5cc7a47035f14408e
Rohit <chotukr488@gmail.com>
2023-09-13 17:41:45 +0530

    Apna Bucket

---
+++ js/shopItems.js
@@ -0,0 +1,183 @@
+let storeItems =  [
+
+    row1_Left = [
+        {
+            id:"lslkdjfls",
+            name: "Combo Chocolates 3 Dairy Milk+1 Kikat+1Snicker", 
+            price: 700, 
+            img: "../items-img/combo1.png"
+        },
+        {
+            id:"lalksjdhf",
+            name: "All Combo More Sweet", 
+            price: 1400, 
+            img: "../items-img/combo2.png"
+        },
+        {
+            id:"owiehfjshf",
+            name: "Combo 7 dairy Milk+4 five Start+4Kitkat", 
+            price: 2500, 
+            img: "../items-img/combo3.jpg"
+        },
+    ],
+    row1_Right = [
+        {
+            id:"pslsalsld",
+            name: "Cadbury 5 Start 200g", 
+            price: 250, 
+            img: "../items-img/fivestart1.webp"
+        },
+        {
+            id:"xslalxld",
+            name: "Keto Chocolate Pack of 4", 
+            price: 410, 
+            img: "../items-img/Keto1.png"
+        },
+        {
+            id:"qswoawwos",
+            name: "Kitkat 3 Pack", 
+            price: 1900, 
+            img: "../items-img/kit1.png"
+        },
+        {
+            id:"abcdeslksdf",
+            name: "Brawine Chocolate", 
+            price: 3400, 
+            img: "../items-img/Browine1.png"
+        },
+        {
+            id:"bcdelslkdf",
+            name: "Dark Fantasy 3 Pack", 
+            price: 700, 
+            img: "../items-img/dark4.jpg"
+
+        },
+        {
+            id:"deslkdfdf",
+            name: "Hot Drink Chocolate", 
+            price: 99, 
+            img: "../items-img/choclate2.jpg"
+        },
+        {
+            id:"lalasdkjhsk",
+            name: "Solid Milk Chocolate", 
+            price: 159, 
+            img: "../items-img/choclate1.jpg"
+        },
+        {
+            id:"aakskdjfds",
+            name: "Mocka Chocolate", 
+            price: 79, 
+            img: "../items-img/choco1.jpg"
+        },
+        {
+            id:"skdkfvjhndf",
+            name: "Kitkat ", 
+            price: 99, 
+            img: "../items-img/kit8.webp"
+        },
+        
+    ],
+    row2 = [
+        {
+            id:"efesfkjd",
+            name: "Keto Chcolate", 
+            price: 150, 
+            img: "../items-img/keto2.png"
+        },
+        {
+            id:"ghalsdkf",
+            name: "Dark Fantasy 3 Pack", 
+            price: 199, 
+            img: "../items-img/dark2.png"
+        },
+        {
+            id:"isldijhfs",
+            name: "Kitkat", 
+            price: 800, 
+            img: "../items-img/kit2.png"
+        },
+        {
+            id:"klaksdjf",
+            name: "Moments", 
+            price: 1800, 
+            img: "../items-img/moments1.png"
+        },
+        {
+            id:"skdkjhfsd",
+            name: "Keto", 
+            price: 99, 
+            img: "../items-img/keto3.png"
+        },
+        {
+            id:"skdjfhksjf",
+            name: "Kitkat Dark Chocolate", 
+            price: 179, 
+            img: "../items-img/kit7.webp"
+        },
+        {
+            id:"skskdfjhs",
+            name: "Dark Fantasy", 
+            price: 49, 
+            img: "../items-img/dark3.png"
+        },
+        {
+            id:"skdkjfhskdj",
+            name: "Mochup", 
+            price: 120, 
+            img: "../items-img/choco2.jpg"
+        }
+    ],
+    row3 = [
+        {
+            id:"bvvssdfsd",
+            name: "Mochup", 
+            price: 110, 
+            img: "../items-img/choco3.jpg"
+        },
+        {
+            id:"wiksiwksk",
+            name: "Moments 500g", 
+            price: 1300, 
+            img: "../items-img/moments2.png",
+        },
+        {
+            id:"zxxsskjd",
+            name: "Bar Dairy Milk 5 bar", 
+            price: 124, 
+            img: "../items-img/dairy.png"
+        },
+        {
+            id:"sslcertificate",
+            name: "Dairy Milk 4 Bar", 
+            price: 39, 
+            img: "../items-img/dairy5.jpg"
+        },
+        {
+            id:"Godisgood",
+            name: "Kitkat", 
+            price: 29, 
+            img: "../items-img/kit4.jpg"
+        },
+        {
+            id:"JesusisLord",
+            name: "Combo Chocolates", 
+            price: 1200, 
+            img: "../items-img/combo3.jpg"
+        },
+        {
+            id:"ThankyouLord",
+            name: "Dairy Milk 56 Bar Pack", 
+            price: 999, 
+            img: "../items-img/dairy6.webp"
+        },
+        {
+            id:"dlslkdjfl",
+            name: "Combo Chocolates", 
+            price: 1200, 
+            img: "../items-img/combo3.jpg"
+        },
+        
+    ],
+
+]
